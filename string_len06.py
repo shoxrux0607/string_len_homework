260b1332b622bdc30fdec370fbae984cc29538bc
shoxrux0607 <chullievshoxrux0607@gmail.com>
2023-01-01 23:14:12 +0500

    06

--- string_len06.py
+++ string_len06.py
@@ -1,10 +1,19 @@
-def main(s1,s2):
+def main(s1, s2):
     """
-    Given two strings, s1 and s2. Return the shortest length between them.
+    Given two strings, s1 and s2. 
+    Return the shortest length between them.
     Args:
         s1: string
         s2: string
     Returns:
         shortest string
     """
-    return
+    a = len(s1)
+    b = len(s2)
+    if a < b:
+        return s1
+    else:
+        return s2
+
+
+print(main('code', 'exam'))

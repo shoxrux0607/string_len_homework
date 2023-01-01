f0e8c3c58d8e599aad9a8c6bb6cfe4dca172ba11
shoxrux0607 <chullievshoxrux0607@gmail.com>
2023-01-01 21:39:17 +0500

    03

--- string_len03.py
+++ string_len03.py
@@ -1,10 +1,18 @@
-def main(a,b):
+def main(a, b):
     """
-    String type variables a and b are given. Return True if the length is equal. If not equal, return False.
+    String type variables a and b are given.
+     Return True if the length is equal.
+      If not equal, return False.
     Args:
         a: string
         b: string
     Returns:
         True or False
     """
-    return
+    if len(a) == len(b):
+        return True
+    else:
+        return False
+
+
+print(main('exam', 'codes'))

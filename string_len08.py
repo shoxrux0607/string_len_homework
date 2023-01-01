11b5ba6f804a00573511eb07c9a6639aa6556f5d
shoxrux0607 <chullievshoxrux0607@gmail.com>
2023-01-02 01:05:38 +0500

    08

--- string_len08.py
+++ string_len08.py
@@ -1,11 +1,26 @@
+from math import *
+
+
 def main(s):
     """
-    Given variable type string s. Return the character in the middle.
-    If the length is even, return two characters in the middle.
+    Given variable type string s. 
+    Return the character in the middle.
+    If the length is even, 
+    return two characters in the middle.
 
     Args:
         s: str
     Returns:
         str: answer
     """
-    return
+
+    lens = len(s)
+    if lens % 2 == 0:
+        harf = s[floor(lens/2-1):floor(lens/2+1)]
+
+    else:
+        harf = s[floor(lens/2)]
+    return harf
+
+
+print(main("askotpsd"))

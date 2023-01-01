f4e5a608a118a2309211da40c2fe8738a59c1931
shoxrux0607 <chullievshoxrux0607@gmail.com>
2023-01-02 00:44:45 +0500

    007

--- string_len07.py
+++ string_len07.py
@@ -12,28 +12,28 @@ def main(s1, s2, s3):
     """
 
     satr = ""
-   # print(satr)
+   
     len_1 = len(s1)
     len_2 = len(s2)
     len_3 = len(s3)
 
     if len_1 % 2 != 0:
-        if len(satr)==0:
+        if len(satr) == 0:
             satr += s1
-       
+
     if len_2 % 2 != 0:
-        if len(satr)==0:
+        if len(satr) == 0:
             satr += s2
         else:
-             satr +=","+ s2
+            satr += ", " + s2
 
     if len_3 % 2 != 0:
-        if len(satr)==0:
+        if len(satr) == 0:
             satr += s3
         else:
-            satr +=","+ s3
- 
+            satr += ", " + s3
+
     return f"[{satr}]"
 
 
-print(main("code", "pythonn", "code"))
+print(main("codeschool.uz", "example", "python"))

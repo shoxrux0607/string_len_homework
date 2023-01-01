12e29c5747c974441b2b7ea54508675df466317b
shoxrux0607 <chullievshoxrux0607@gmail.com>
2023-01-01 23:10:10 +0500

    05

--- string_len05.py
+++ string_len05.py
@@ -1,4 +1,4 @@
-def main(s1,s2):
+def main(s1, s2):
     """
     Given two strings, s1 and s2. Find their total length.
     Args:
@@ -7,4 +7,7 @@ def main(s1,s2):
     Returns:
         total length of strings
     """
-    return
+    return len(s1)+len(s2)
+
+
+print(main('salom ', 'alik'))

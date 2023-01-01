b8741defce67ecb2868a7ce4421b64fe287e5bc2
shoxrux0607 <chullievshoxrux0607@gmail.com>
2023-01-01 18:45:35 +0500

    02

--- string_len02.py
+++ string_len02.py
@@ -1,9 +1,17 @@
 def main(a):
     """
-    A string type variable is given. Return True if its length is even. Return False if its length is odd.
+    A string type variable is given. 
+    
+    Return True if its length is even.
+     Return False if its length is odd.
     Args:
         a: string
     Returns:
         True or False
     """
-    return
+    if len(a)%2==0:
+        return True
+    else:
+        return False
+
+print(main("sal"))

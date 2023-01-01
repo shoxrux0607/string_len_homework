dabc79f0ece6c6a10b6bc08bee554088a46f06d3
shoxrux0607 <chullievshoxrux0607@gmail.com>
2023-01-01 21:44:06 +0500

    04

--- string_len04.py
+++ string_len04.py
@@ -1,9 +1,14 @@
 def main(s):
     """
-    A string variable s is given. Return the "*" sign that is equal to the length of this variable.
+    A string variable s is given. Return the "*" 
+    sign that is equal to the length of this variable.
     Args:
         s: string
     Returns:
         string
     """
-    return
+
+    return len(s)*'*'
+
+
+print(main('codess'))
